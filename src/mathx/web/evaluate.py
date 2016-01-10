d02f66ad9f25443eb9c75e730c807c3cc599a483
michi <michaelglas89@gmail.com>
2016-01-10 20:14:43 +0100

    xxx.

--- src/mathx/web/evaluate.py
+++ src/mathx/web/evaluate.py
@@ -16,34 +16,46 @@ NUMBER_TYPES = (int,float)
 
 STATE_KEYS = {"n":int,"xmin":NUMBER_TYPES,"xmax":NUMBER_TYPES,"ymin":NUMBER_TYPES,"ymax":NUMBER_TYPES,"f":str}
 
-def valuesgen(state):
-    f = state["f"]
-    xmin = state["xmin"]
-    xmax = state["xmax"]
-    ymin = state["ymin"]
-    ymax = state["ymax"]
-    n = state["n"]
-    n1 = n-1
-    
-    ast = formula.Parser(f).parseAst()
-    vvars = list(ast.findVars())
-    assert len(vvars)==2
-    
-    for i in range(n):
+class valuesiter(list):
+    def __init__(self,state):
+        self.f = state["f"]
+        self.xmin = state["xmin"]
+        self.xmax = state["xmax"]
+        self.ymin = state["ymin"]
+        self.ymax = state["ymax"]
+        self.n = state["n"]
+        self.n1 = self.n-1
         
-        x = xmin + i*(xmax-xmin)/n1
-        
-        for j in range(n):
-            y = ymin + j*(ymax-ymin)/n1
-            try:
-                z = ast.evaluate({vvars[0]:x,vvars[1]:y})
-            except:
-                z = None
+        self.ast = formula.Parser(self.f).parseAst()
+        self.vvars = list(self.ast.findVars())
+        assert len(self.vvars)==2
+        self._i = 0
+        self._j = 0
+    def __next__(self):
+        if self._i<self.n:
+            x = self.xmin + self._i*(self.xmax-self.xmin)/self.n1
             
-            if isinstance(z, (int,float)) and (math.isnan(z.real) or math.isinf(z.real)):
-                yield None
+            if self._i<self.n:
+                y = self.ymin + self._j*(self.ymax-self.ymin)/self.n1
+                try:
+                    z = self.ast.evaluate({self.vvars[0]:x,self.vvars[1]:y})
+                except:
+                    z = None
+                
+                if isinstance(z, (int,float)) and (math.isnan(z.real) or math.isinf(z.real)):
+                    yield None
+                else:
+                    yield z
+                self._j += 1
             else:
-                yield z
+                self._j = 0
+            self._i += 1
+        else:
+            raise StopIteration()
+    def __iter__(self):
+        return self
+    def __len__(self):
+        return 1
 
 def handler(env, start_response):
     
@@ -58,9 +70,9 @@ def handler(env, start_response):
         assert isinstance(value, vtype)
         ret[key] = value
     
-    values = valuesgen(state)
+    values = valuesiter(ret)
 
-    ret["values"] = type("StreamArray",(list,),{"__iter__":lambda self:values,"__len__":lambda self: 1})()
+    ret["values"] = values
     
     start_response('200 OK', [('Content-Type','application/json')])
     je = json.JSONEncoder()
